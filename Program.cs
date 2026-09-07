aa826ef299dbc6feeb963d7e28b5f733d5ecc2c8
Andre’ Abrahamsen <180651878+topandre@users.noreply.github.com>
2026-09-07 13:55:00 +0200

    Add input validation

--- Program.cs
+++ Program.cs
@@ -10,8 +10,16 @@ while (running)
 
     if (menuChoice == "1")
     {
+        
         Console.WriteLine("Which subject do you want to study?");
         string? subject = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(subject))
+        {
+            Console.WriteLine("Subject is empty. Please try again.");
+            Console.WriteLine("Which subject do you want to study?");
+            subject = Console.ReadLine();
+        }
+            
 
         Console.WriteLine("Press Enter to start the session");
         Console.ReadLine();
@@ -28,6 +36,10 @@ while (running)
     else if (menuChoice == "2")
     {
         running = false;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice. Please try again.");
     }
     
     
