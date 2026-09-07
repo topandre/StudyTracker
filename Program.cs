ee898e7f38b4240d1d10ebcb2a26b42c594745f7
Andre’ Abrahamsen <180651878+topandre@users.noreply.github.com>
2026-09-07 13:11:30 +0200

    Add menu and study session timer

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
 ﻿Console.WriteLine("Study Tracker");
+
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("1. Start study session");
+    Console.WriteLine("2. Exit");
+    string? menuChoice = Console.ReadLine();
+
+    if (menuChoice == "1")
+    {
+        Console.WriteLine("Which subject do you want to study?");
+        string? subject = Console.ReadLine();
+
+        Console.WriteLine("Press Enter to start the session");
+        Console.ReadLine();
+        DateTime startTime = DateTime.Now;
+        Console.WriteLine("Session started.");  
+
+        Console.WriteLine("Press Enter to stop the session");
+        Console.ReadLine();
+        DateTime endTime = DateTime.Now;
+        TimeSpan duration = endTime - startTime;
+
+        Console.WriteLine($"You studied {subject} for {duration.Hours}h {duration.Minutes}m {duration.Seconds}s");
+    }
+    else if (menuChoice == "2")
+    {
+        running = false;
+    }
+    
+    
+   
+
+}
+
+
